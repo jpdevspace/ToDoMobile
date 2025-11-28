7ee3dd4752c5c88d27f2f29194bdfc3796e79880
Jean Paul Giraldo <jpgiraldo7@gmail.com>
2025-11-28 16:23:44 -0500

    Displaying new todos

--- App.js
+++ App.js
@@ -1,30 +1,61 @@
+import { useState } from "react";
 import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function todoInputHandler(enteredText) {
+    setNewTodo(enteredText);
+  }
+
+  function addTodoHandler() {
+    setTodos((currentTodos) => [...currentTodos, newTodo]);
+    setNewTodo("");
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Type your To Do" />
-        <Button title="Add Item"></Button>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Type your To Do"
+          onChangeText={todoInputHandler}
+        />
+        <Button title="Add Item" onPress={addTodoHandler}></Button>
       </View>
-      <View></View>
+      <View style={styles.todosContainer}>
+        {todos.map((todo, i) => (
+          <Text key={`${todo}-${i}`}>{todo}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
-    padding: 2,
+    padding: 8,
+  },
+  todosContainer: {
+    flex: 5,
   },
 });
